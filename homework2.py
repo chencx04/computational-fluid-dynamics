65956ed6c881fcdf033abbd8379735b6d9582874
chencx04 <1771930680@qq.com>
2026-05-09 20:54:34 +0800

    求解差分格式系数部分的debug

--- homework2.py
+++ homework2.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 def solve_coefficient():
-    a = np.array(
+    a = np.array([
         [1,0,0,0,0],
         [1,1,1,1,1],
         [1,2,4,8,16],
         [1,3,9,27,81],
-        [1,4,16,64,256])
+        [1,4,16,64,256]])
 
     b = np.array([0,1,0,0,0])
 
