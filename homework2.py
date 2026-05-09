92742d322da1602b2275ef7d3d62365301a9dee2
chencx04 <1771930680@qq.com>
2026-05-09 20:51:51 +0800

    用于得到5节点模板的单边差分格式系数

---
+++ homework2.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def solve_coefficient():
+    a = np.array(
+        [1,0,0,0,0],
+        [1,1,1,1,1],
+        [1,2,4,8,16],
+        [1,3,9,27,81],
+        [1,4,16,64,256])
+
+    b = np.array([0,1,0,0,0])
+
+    c = np.linalg.solve(a,b)
+
+    print(c)
+
+solve_coefficient()
