9f2120afe9c86c0636c56dd99c91abb79086f20d
chencx04 <1771930680@qq.com>
2026-05-09 22:33:37 +0800

    1、绘制差分格式误差-网格步长的双对数图，通过拟合计算差分格式的误差阶数；2、修改已经存在的method，使其能接收输入

--- homework2.py
+++ homework2.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from scipy import stats
 
 def solve_coefficient():
     a = np.array([
@@ -20,9 +21,9 @@ def solve_coefficient():
 # solve_coefficient()
 
 
-def plot_error_for_fdm():
+def plot_error_for_fdm(isplot: int = 1, h_array: np.ndarray = np.linspace(0.05,1.05,101)):
     # 定义一个步长 h 的数组
-    h_array = np.linspace(0.05,1.05,101)
+    # h_array = np.linspace(0.05,1.05,101)
     # 函数 f(x)
     f = lambda x: np.exp(2*x)*2
     # 解析形式的导数 f'(x = -1)
@@ -42,14 +43,46 @@ def plot_error_for_fdm():
         f_prime3[i] = (-3*f(x0) + 4*f(x1) -f(x2))/(2*h)
         # 5 节点模板的有限差分格式
         f_prime5[i] = (-25*f(x0) + 48*f(x1) - 36*f(x2) + 16*f(x3) - 3*f(x4))/(12*h)
+    
     # plot error-h figure
-    plt.plot(h_array, f_prime3, label='3-point')
-    plt.plot(h_array, f_prime5, label='5-point')
-    plt.plot(h_array, f_prime_analytical, label='analytical')
-    plt.xlabel('h')
-    plt.ylabel('f_prime')
+    if isplot:
+        plt.plot(h_array, f_prime3, label='3-point')
+        plt.plot(h_array, f_prime5, label='5-point')
+        plt.plot(h_array, f_prime_analytical, label='analytical')
+        plt.xlabel('h')
+        plt.ylabel('f_prime')
+        plt.legend()
+        plt.savefig('result.png')
+
+    return h_array, f_prime3, f_prime5, f_prime_analytical
+
+# plot_error_for_fdm()
+
+def calculate_order_of_accuracy_for_fdm(h_array):
+    h_array, f_prime3, f_prime5, f_prime_analytical = plot_error_for_fdm(0, h_array)
+    # 绘制 log-log 图，以确定误差阶数
+    log_h_array = np.log(h_array)
+    log_error_prime3 = np.log(abs(f_prime3 - f_prime_analytical))
+    log_error_prime5 = np.log(abs(f_prime5 - f_prime_analytical))
+    # 拟合
+    slope3, intercept3, r_value3, _, _ = stats.linregress(log_h_array, log_error_prime3)
+    slope5, intercept5, r_value5, _, _ = stats.linregress(log_h_array, log_error_prime5)
+
+    # 画图以可视化
+    plt.close('all')
+    plt.plot(log_h_array, log_error_prime3, label='3-point, r = %.4f, slope = %.4f' % (r_value3,slope3))
+    plt.plot(log_h_array, log_error_prime5, label='5-point, r = %.4f, slope = %.4f' % (r_value5,slope5))
+    plt.xlabel('log(h)')
+    plt.ylabel('log(error)')
+    plt.title('h = %.4f to %.4f' % (h_array[0], h_array[len(h_array)-1]))
     plt.legend()
-    plt.savefig('result.png')
+    plt.savefig('order_of_accuracy %.4f %.4f.png' % (h_array[0], h_array[len(h_array)-1]))
 
-plot_error_for_fdm()
-
+h_array = np.linspace(0.05,1.05,101)
+calculate_order_of_accuracy_for_fdm(h_array)
+h_array = np.linspace(0.01,0.51,101)
+calculate_order_of_accuracy_for_fdm(h_array)
+h_array = np.linspace(0.001,0.101,101)
+calculate_order_of_accuracy_for_fdm(h_array)
+h_array = np.linspace(0.0001,0.0101,101)
+calculate_order_of_accuracy_for_fdm(h_array)
